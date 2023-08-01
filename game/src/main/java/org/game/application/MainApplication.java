51a38008f42adb11749d14e9ae961fe71a8420f6
Emirhan Bulut <emirhanblt9@gmail.com>
2023-08-01 20:30:06 +0300

    Dosyanın yeri değiştirildi.

--- game/src/main/java/org/game/application/MainApplication.java
+++ game/src/main/java/org/game/application/MainApplication.java
@@ -1,4 +1,4 @@
-package org.game.server.application;
+package org.game.application;
 
 import com.sun.net.httpserver.HttpServer;
 import org.game.entities.game.session.Room;
@@ -6,10 +6,12 @@ import org.game.entities.game.session.User;
 
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
 
 public class MainApplication {
     public static ArrayList<User> users;
     public static ArrayList<Room> rooms;
+
     public static void main(String[] args) throws Exception {
         final int port = 8080;
         HttpServer GameServer = HttpServer.create(new InetSocketAddress(port),0);
