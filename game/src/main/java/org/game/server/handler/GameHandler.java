c37bbd938dd93e5aa169a6972ad2395205ab951d
Emirhan Bulut <emirhanblt9@gmail.com>
2023-08-01 20:38:16 +0300

    Handler'a yeni message handler eklendi.

--- game/src/main/java/org/game/server/handler/GameHandler.java
+++ game/src/main/java/org/game/server/handler/GameHandler.java
@@ -8,7 +8,7 @@ import org.game.manager.UserManager;
 import org.game.entities.game.session.Room;
 import org.game.entities.game.session.User;
 import org.game.manager.RoomManager;
-import org.game.server.application.MainApplication;
+import org.game.application.MainApplication;
 
 
 import java.io.IOException;
@@ -31,7 +31,7 @@ public class GameHandler implements HttpHandler {
 
             //message="CREATE_USER:'CUSTOM_NICKNAME'"
             if(codes[0] == "CREATE_USER"){
-                User user = this.userManager.createUser(hostIP,codes[1]);
+                User user = this.userManager.createUser(hostIP,codes[1]);//todo after web socket
                 MainApplication.users.add(user);
             }
             //message="CREATE_ROOM:'ROOM_ID':MAXIMIZER"
@@ -50,6 +50,10 @@ public class GameHandler implements HttpHandler {
                         Maps.newHashMap()
                 );
                 MainApplication.rooms.add(room);
+            }
+            //message="JOIN_ROOM:'ROOM_ID'"
+            else if (true) {
+
             }
 
 
