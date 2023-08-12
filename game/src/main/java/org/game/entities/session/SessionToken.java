5d5a6861a7cb1a3bf5f39b542c152f3ad2cbc425
Emirhan Bulut <emirhanblt9@gmail.com>
2023-08-12 10:34:19 +0300

    Sınıf oluşturuldu

---
+++ game/src/main/java/org/game/entities/session/SessionToken.java
@@ -0,0 +1,29 @@
+package org.game.entities.session;
+
+import java.time.LocalDateTime;
+
+public class SessionToken {
+    private String tokenContent;
+    private LocalDateTime expirationTime;
+
+    public SessionToken(String tokenContent, LocalDateTime expirationTime) {
+        this.tokenContent = tokenContent;
+        this.expirationTime = expirationTime;
+    }
+
+    public String getTokenContent() {
+        return tokenContent;
+    }
+
+    public void setTokenContent(String tokenContent) {
+        this.tokenContent = tokenContent;
+    }
+
+    public LocalDateTime getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(LocalDateTime expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
