33f2feada5fe1fcf489094dbdaac50d5df878dda
Emirhan Bulut <emirhanblt9@gmail.com>
2023-08-17 12:25:11 +0300

    Login methodu oluşturuldu ve yeni oluşturulan exceptionlar ile try-catch yapıları kullanan yeni bir yapı oluşturuldu. Sorun çözüldü.

--- game/src/main/java/org/game/server/handler/LoginHandler.java
+++ game/src/main/java/org/game/server/handler/LoginHandler.java
@@ -3,6 +3,7 @@ package org.game.server.handler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.game.application.MainApplication;
+import org.game.common.util.exceptions.AlreadyExistsException;
 import org.game.entities.game.session.User;
 import org.game.entities.session.SessionToken;
 import org.game.manager.RoomManager;
@@ -29,8 +30,8 @@ public class LoginHandler extends BaseHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String successful = "Login Successful!";
         OutputStream os = exchange.getResponseBody();
+
         //Getting the method
         String requestMethod = exchange.getRequestMethod();
         //Getting the message and requestIP
@@ -39,14 +40,15 @@ public class LoginHandler extends BaseHandler implements HttpHandler {
         if(requestMethod.equals("POST")){
             if(messageList[0].equals("LOGIN")){
                 String username = messageList[1];
-                System.out.println("User " + username + " joined to server");
-                SessionToken token = sessionTokenManager.generateToken();
-                exchange.getResponseHeaders().add("Authorization",token.getTokenContent());
-                exchange.sendResponseHeaders(200,successful.length());
-                os.write(successful.getBytes(StandardCharsets.UTF_8));
-                os.close();
+                try{
+                    User user = login(exchange,userManager,username);
+                    System.out.println("User " + username + " joined to server");
+                    userManager.addUserToList(user); //Added to list of user
+                }catch (Exception e){
+                    exchange.sendResponseHeaders(409,e.getMessage().length());
+                    os.write(e.getMessage().getBytes(StandardCharsets.UTF_8));
+                }
             }
-
         } else if (requestMethod.equals("GET")) {
             Path file = Paths.get("./src/main/resources/login/login.html");
             try {
@@ -54,6 +56,22 @@ public class LoginHandler extends BaseHandler implements HttpHandler {
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+    private User login(HttpExchange exchange, UserManager userManager, String username) throws AlreadyExistsException, IOException {
+        String IP = exchange.getRemoteAddress().getAddress().toString();
+        String successfulMessage = "Login Successful!";
+        OutputStream os = exchange.getResponseBody();
+        if(checkIfNameExists(username)){
+            throw new AlreadyExistsException();
+        }else{
+            userManager.checkExpirations();
+            SessionToken token = sessionTokenManager.generateToken();
+            //Response
+            exchange.getResponseHeaders().add("Authorization",token.getTokenContent());
+            exchange.sendResponseHeaders(200,successfulMessage.length());
+            os.write(successfulMessage.getBytes(StandardCharsets.UTF_8));
+            return userManager.createUser(IP,username,token);
         }
     }
 }
